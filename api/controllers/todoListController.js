db1715ae6d06d4b45074cf152fde2741c4c974a0
esarachik <esarachik@VPUY-ESARACHIK2>
2017-09-13 11:21:03 -0300

    Arreglo

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -1,9 +1,5 @@
 'use strict';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 13042107258e9cc9ba051645dba1023c5e5e686e
 var mongoose = require('mongoose'),
   Task = mongoose.model('Tasks');
 
@@ -15,12 +11,6 @@ exports.list_all_tasks = function(req, res) {
   });
 };
 
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 13042107258e9cc9ba051645dba1023c5e5e686e
 exports.create_a_task = function(req, res) {
   var new_task = new Task(req.body);
   new_task.save(function(err, task) {
@@ -30,10 +20,6 @@ exports.create_a_task = function(req, res) {
   });
 };
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 13042107258e9cc9ba051645dba1023c5e5e686e
 exports.read_a_task = function(req, res) {
   Task.findById(req.params.taskId, function(err, task) {
     if (err)
@@ -42,10 +28,6 @@ exports.read_a_task = function(req, res) {
   });
 };
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 13042107258e9cc9ba051645dba1023c5e5e686e
 exports.update_a_task = function(req, res) {
   Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
     if (err)
@@ -54,14 +36,7 @@ exports.update_a_task = function(req, res) {
   });
 };
 
-<<<<<<< HEAD
 exports.delete_a_task = function(req, res) {
-=======
-
-exports.delete_a_task = function(req, res) {
-
-
->>>>>>> 13042107258e9cc9ba051645dba1023c5e5e686e
   Task.remove({
     _id: req.params.taskId
   }, function(err, task) {
@@ -70,7 +45,3 @@ exports.delete_a_task = function(req, res) {
     res.json({ message: 'Task successfully deleted' });
   });
 };
-<<<<<<< HEAD
-
-=======
->>>>>>> 13042107258e9cc9ba051645dba1023c5e5e686e
